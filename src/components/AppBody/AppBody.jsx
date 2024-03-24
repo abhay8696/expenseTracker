cd4a55b9feeb61468bb8d61f95666c7752af5d1f
abhay8696 <kambleabhaygautam@gmail.com>
2024-03-24 23:50:55 +0530

    created new TopExpense and TopExpenseBody components

--- src/components/AppBody/AppBody.jsx
+++ src/components/AppBody/AppBody.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 //styles
 import "./AppBody.css";
 import Transactions from '../Transactions/Transactions';
+import TopExpenses from '../TopExpenses/TopExpenses';
 //components
 
 const AppBody = () => {
     return (
         <div className='AppBody'>
-            <Transactions />
+            {/* <Transactions /> */}
+            <TopExpenses />
         </div>
     );
 };
---
+++ src/components/TopExpenseBody/TopExpenseBody.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//styles
+import "../TransactionsBody/TransactionsBody.css";
+
+const TopExpenseBody = () => {
+    return (
+        <div className='TopExpensesBody'>
+        </div>
+    );
+};
+
+export default TopExpenseBody;
