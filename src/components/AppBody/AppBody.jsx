8fb18076398b571b310b310a8251842599c3b8bf
abhay8696 <kambleabhaygautam@gmail.com>
2024-03-24 14:29:01 +0530

    created AppBody component

---
+++ src/components/AppBody/AppBody.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//styles
+import "./AppBody.css";
+
+const AppBody = () => {
+    return (
+        <div className='AppBody'>
+            
+        </div>
+    );
+};
+
+export default AppBody;
