a0eec53158e2564d38a3ef5e5865ac901a10e97d
abhay8696 <kambleabhaygautam@gmail.com>
2024-03-24 13:13:17 +0530

    created PieChart component and created temporary circle shaped div to represent pie chart

--- src/components/PieChart/PieChart.jsx
+++ src/components/PieChart/PieChart.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+//styles
+import "./PieChart.css";
+//components
 import PieLabel from '../PieLabel/PieLabel';
 
 const PieChart = () => {
     return (
-        <div>
+        <div className='pieChart'>
+            <div className='pie'>
+        
+            </div>
             <PieLabel />
         </div>
     );
