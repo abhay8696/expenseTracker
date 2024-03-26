9317f8b7c6c1357d04b996d6ddd53f63a82b5303
abhay8696 <kambleabhaygautam@gmail.com>
2024-03-26 15:28:28 +0530

    created new state variable object named money to store balance and expense amounts and created context of it

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,33 @@
 import { useState } from 'react'
 //styles
 import './App.css';
-import foodIcon from "./assets/food.svg";
-import movieIcon from "./assets/movie.svg";
-import travelIcon from "./assets/travel.svg";
 //components
 import Navbar from './components/Navbar/Navbar'
 import AppHead from './components/AppHead/AppHead'
 import AppBody from './components/AppBody/AppBody';
 //contexts
-import { TransactionsContext } from "./Contexts/AllContexts"
+import { TransactionsContext, MoneyContext } from "./Contexts/AllContexts"
+//variables
+import { dummyData } from './dummyTransactions';
 
-const dummyData = [
-  {
-    name: "Movie",
-    date: "March 20 2024",
-    category: "entertainment",
-    price: 300,
-    icon: movieIcon
-  },
-  {
-    name: "Samosa",
-    date: "March 21 2024",
-    category: "food",
-    price: 50,
-    icon: foodIcon
-  },
-  {
-    name: "Auto",
-    date: "March 19 2024",
-    category: "travel",
-    price: 150,
-    icon: travelIcon
-  },
-]
 function App() {
-  const [balance, setBalance] = useState(5000);
-  const [expenses, setExpenses] = useState(0);
+  // const [balance, setBalance] = useState(5000);
+  // const [expenses, setExpenses] = useState(0);
+  const [money, setMoney] = useState({
+    balance: 5000,
+    expenses: 0
+  })
   const [transactionData, setTransactionData] = useState(dummyData)
 
   return (
     <main className='App'>
+      <MoneyContext.Provider value={[money, setMoney]}>
       <TransactionsContext.Provider value={[transactionData, setTransactionData]}>
         <Navbar />
-        <AppHead balance={balance} expenses={expenses}/>
+        <AppHead balance={money.balance} expenses={money.expenses}/>
         <AppBody transactionData={transactionData}/>
       </TransactionsContext.Provider> 
+      </MoneyContext.Provider>
     </main>
   )
 }
---
+++ src/dummyTransactions.js
@@ -0,0 +1,29 @@
+
+import foodIcon from "./assets/food.svg";
+import movieIcon from "./assets/movie.svg";
+import travelIcon from "./assets/travel.svg";
+
+export const dummyData = [
+    {
+      name: "Movie",
+      date: "March 20 2024",
+      category: "entertainment",
+      price: 300,
+      icon: movieIcon
+    },
+    {
+      name: "Samosa",
+      date: "March 21 2024",
+      category: "food",
+      price: 50,
+      icon: foodIcon
+    },
+    {
+      name: "Auto",
+      date: "March 19 2024",
+      category: "travel",
+      price: 150,
+      icon: travelIcon
+    },
+  ]
+
