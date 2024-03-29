a1b01343a253be6584a6529a21d8ff420b1385ee
abhay8696 <kambleabhaygautam@gmail.com>
2024-03-29 21:03:26 +0530

    created function to sort chartData to show on topExpenses bar chart

--- src/components/TopExpenseBody/TopExpenseBody.jsx
+++ src/components/TopExpenseBody/TopExpenseBody.jsx
@@ -13,11 +13,16 @@ const TopExpenseBody = () => {
             { name: 'entertainment', value: 0 },
             { name: 'travel', value: 0 },
         ]);
-
+    
+    //functions
+    const showSortedData = () => {
+        return chartData.sort((a,b) => b.value - a.value);
+    }
+    
     return (
-        <div className='TopExpensesBody' style={{height: "100px"}}>
+        <div className='TopExpensesBody' style={{height: "100px"}} onClick={showSortedData}>
             <ResponsiveContainer width="100%" height="100%" >
-                <BarChart data={chartData} layout="vertical" barSize={30}>
+                <BarChart data={showSortedData()} layout="vertical" barSize={30}>
                     <XAxis type='number' hide/>
                     <YAxis type="category" width={120} dataKey="name"/>
                     <Bar dataKey="value" fill="#8784D2" />
